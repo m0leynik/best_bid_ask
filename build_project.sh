edc186010bba2d609237bcb60d2385d19dc62b6d
m0leynik <m0leynik@github.com>
2022-06-13 22:46:37 +0300

    Use g++ instead of g++10 in build script

--- build_project.sh
+++ build_project.sh
@@ -1,2 +1,2 @@
-cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++-10 -DCMAKE_BUILD_TYPE=Release -B ./build
+cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_BUILD_TYPE=Release -B ./build
 cmake --build ./build
